19d6e852489264a2c43c3f9ba30fd6024aad5674
28add11 <kerbal2346@gmail.com>
2023-05-23 20:18:15 -0600

    add disease data basics

---
+++ disease.py
@@ -0,0 +1,8 @@
+import pygame
+
+diseaseData = {"Maggots" : {"Symptoms" : ["Worms", "Flies", "Bad things"], 
+                            "Treatments" : {"Picking them off" : "Flesh Decay", "3 Ounces of Pig guts" : "Death", "Nothing" : "Excruciating Pain"}}, 
+                            "Cats" : {"Symptoms" : ["Cuteness", "Awesomeness", "omg"], }}
+
+symptomDialouge = {"Worms" : ["There are these little white things crawling on my skin, and they seem to be eating my flesh",
+                               "Grayson for the love of god use chatgpt for these lines"]}

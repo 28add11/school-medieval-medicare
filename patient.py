6cf26cbd7a44e078bb9cc1dd239bb1b18afc277e
28add11 <kerbal2346@gmail.com>
2023-05-26 16:38:07 -0600

    added patient sprite info, need sprites to test

--- patient.py
+++ patient.py
@@ -1,5 +1,16 @@
 import pygame
 from disease import disease
+from random import choice
+import os
+
+
+def getSubfolders(folder_path):
+    subfolders = []
+    for entry in os.scandir(folder_path):
+        if entry.is_dir():
+            subfolders.append(entry.path)
+    return subfolders
+
 
 class patient(pygame.sprite.Sprite):
     '''Class for an individual patient, with a list of sprites for various modifiers (eg shirt, visible disease symptoms)'''
@@ -7,7 +18,12 @@ class patient(pygame.sprite.Sprite):
     def __init__(self):
         
         self.disease = disease(True, "", [], 2, False)
-        self.patientsprite = pygame.sprite.Sprite()
 
+        availiblePeople = []
+        availiblePeople.extend(getSubfolders("childSprites"))
+        availiblePeople.extend(getSubfolders("manSprites"))
+        availiblePeople.extend(getSubfolders("womanSprites"))
+
+        self.person = choice(availiblePeople) # Getting a random person from the possible files, then saving a path
         
         pygame.sprite.Sprite.__init__(self)
